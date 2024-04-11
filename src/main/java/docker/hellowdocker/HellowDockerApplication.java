9a6d63b814cde3a5cd85f752ad13da397df9a3f0
pie0902 <zhfoqkq@gmail.com>
2024-04-12 02:34:07 +0900

    git init

---
+++ src/main/java/docker/hellowdocker/HellowDockerApplication.java
@@ -0,0 +1,13 @@
+package docker.hellowdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellowDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/docker/hellowdocker/HellowDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package docker.hellowdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
